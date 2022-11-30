455acc3bd60ade668a099aa6e30dea9b7b037cd6
Rafał Gałecki <darthunter@gmail.com>
2022-11-30 18:02:17 +0100

    Task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+'use strict';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const myGallery = document.querySelector('div.gallery');
+
+createSimpleGalleryItems();
+
+function createSimpleGalleryItems() {
+  const items = galleryItems
+    .map(
+      image =>
+        `<a class="gallery__item" href="${image.original}"><img class="gallery__image" src="${image.preview}" alt="${image.description}"/></a>`
+    )
+    .join('');
+  myGallery.insertAdjacentHTML('beforeend', items);
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+gallery.on();
