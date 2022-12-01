29502a274f2121181f5fd629892cad3bac5c2053
Rafał Gałecki <darthunter@gmail.com>
2022-12-01 09:21:31 +0100

    Task 2 vimeoplayer listener

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+"use strict";
 
+import Player from '@vimeo/player';
+
+const player = new Player('handstick', {
+  id: 19231868,
+  width: 640,
+});
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+
+// //event listener:
+// const onPlay = function (data) {
+//   // data is an object containing properties specific to that event
+// };
+
+// player.on('play', onPlay);
+
+// //to remove event listener:
+// const onPlay = function (data) {
+//   // data is an object containing properties specific to that event
+// };
+
+// player.on('play', onPlay);
+
+// // If later on you decide that you don’t need to listen for play anymore.
+// player.off('play', onPlay);
+
+// // Alternatively, `off` can be called with just the event name to remove all
+// // listeners.
+// player.off('play');
