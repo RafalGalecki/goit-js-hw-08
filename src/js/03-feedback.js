7fe27eddcc42f212324c3dc952d975402755a2e6
Rafał Gałecki <darthunter@gmail.com>
2023-01-05 17:28:02 +0100

    feedback done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,39 @@
 'use strict';
-const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('form');
+const currentData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-window.addEventListener('load', () => {
-  Array.from(form.elements).forEach(el => {
-    if (el.name) {
-      const feedbackFormState = localStorage.getItem(el.name);
-      el.value = feedbackFormState;
-    }
-  });
-});
+window.addEventListener('load', fillForm());
 
-form.addEventListener('input', e => {
+form.addEventListener(
+  'input',
+  throttle(() => {
+    const { email, message } = form.elements;
+    let inputedData = {
+      email: email.value,
+      message: message.value,
+    };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(inputedData));
+  }, 500)
+);
+
+function fillForm() {
+  if (currentData) {
+    Array.from(form.elements).forEach(el => {
+      if (el.name === 'email') {
+        el.value = currentData.email;
+      }
+      if (el.name === 'message') {
+        el.value = currentData.message;
+      }
+    });
+  }
+}
+form.addEventListener('submit', e => {
   e.preventDefault();
-
-  Array.from(form.elements).forEach(el => {
-    if (el.name) {
-      localStorage.setItem(el.name, el.value);
-    }
-  });
+  console.log(currentData);
+  localStorage.clear();
+  form.reset();
 });
- 
