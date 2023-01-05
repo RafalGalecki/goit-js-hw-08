95ed7f3a407d4182a7a01d659f14ec161a797a8d
Rafał Gałecki <darthunter@gmail.com>
2023-01-05 11:58:07 +0100

    error handling added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,29 @@
-"use strict";
+'use strict';
 
 import Player from '@vimeo/player';
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector("#vimeo-player");
+const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
 const STORAGE_KEY = 'videoplayer-current-time';
 
 const videoplayerCurrentTime = ({ seconds } = 0) => {
   localStorage.setItem(STORAGE_KEY, seconds);
-}
+};
 const getVideoplayerCurrentTime = () => {
   return localStorage.getItem(STORAGE_KEY);
 };
 
-player.setCurrentTime(getVideoplayerCurrentTime());
+player.setCurrentTime(getVideoplayerCurrentTime()).catch(function (err) {
+  switch (err.name) {
+    case 'RangeError':
+      // if the time is <0 || > than video's lenght
+      break;
+    default:
+      // other error
+      break;
+  }
+});
 
 player.on('timeupdate', throttle(videoplayerCurrentTime, 1000));
